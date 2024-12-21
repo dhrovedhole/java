ae8fea2dce8d4e12b172262ea486e292e097755b
dhrovedhole <dhruvdhole113@gmail.com>
2024-12-21 22:05:06 +0530

    Simply Printing Employee details
    It was fun simply printing an employee details and getting to know java syntax for future programs

---
+++ Employee.java
@@ -0,0 +1,20 @@
+public class Employee {
+    public static void main(String[] args) {
+        
+        System.out.println("Name of the employee : Dhrove Dhole");
+
+        System.out.println("Employee ID          : 1505");
+
+        System.out.println("Department           : Sales");
+
+        System.out.println("Designation          : Manager");
+
+        System.out.println("Gross Salary         : Rs.90000/-");
+
+        System.out.println("Contact number       : 98765XXXXX");
+
+        System.out.println("Email ID             : dhrovedhole@sales.com");
+
+    }
+    
+}
