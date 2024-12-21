3d6e5af1547867fdebf395f5072f475792489b05
dhrovedhole <dhruvdhole113@gmail.com>
2024-12-21 22:19:44 +0530

    Printing Employee details using variables
    Used variables for printing Employee details in this code

---
+++ Employee_variable.java
@@ -0,0 +1,24 @@
+public class Employee_variable {
+
+    public static void main(String[] args) {
+        
+        String name = "Dhrove Dhole";
+        int empid = 1505;
+        String dept = "Sales";
+        String desg = "Manager";
+        int grossSal = 90000;
+        String number = "98765XXXXX";
+        String email = "dhrovedhole@sales.com";
+
+        System.out.println("\n Details of the employee are : \n");
+        System.out.println("Name of the Employee : "+name);
+        System.out.println("Employee ID          : "+empid);
+        System.out.println("Department           : "+dept);
+        System.out.println("Designation          : "+desg);
+        System.out.println("Gross Salar          : "+grossSal);
+        System.out.println("Contact Number       : "+number);
+        System.out.println("Email ID             : "+email);
+
+    }
+    
+}
